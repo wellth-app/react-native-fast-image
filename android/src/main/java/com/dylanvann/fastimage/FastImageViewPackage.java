f20893490c88db02acd93ddf32af3d8cf4c52dfa
Kyle Weisel <kyleweisel@gmail.com>
2018-02-20 21:37:00 -0500

    Removed unused method

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewPackage.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewPackage.java
@@ -16,11 +16,6 @@ public class FastImageViewPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return null;
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.<ViewManager>singletonList(new FastImageViewManager());
     }
